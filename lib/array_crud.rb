ab3d1c5ae2d14de08695506f6db3a41243c3840b
fortuitous-prototype-dfa5 <lindsay.chen@yale.edu>
2021-05-27 11:49:11 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -5,7 +5,7 @@ end
 def create_an_array
   ["a", "b", "c", "d"]
 end
-p create_an_array
+puts create_an_array
 
 array_1 = ["wow", "I", "am", "really", "learning"]
 def add_element_to_end_of_array(array, element)
