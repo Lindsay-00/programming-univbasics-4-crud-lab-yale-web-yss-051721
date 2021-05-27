bcf29a820940a41d0db7b91ed39dc3b447d34010
fortuitous-prototype-dfa5 <lindsay.chen@yale.edu>
2021-05-27 11:27:52 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,26 +1,33 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+  ["a", "b", "c", "d"]
 end
+p create_an_array
 
 def add_element_to_end_of_array(array, element)
-  
+  array_1 = ["wow", "I", "am", "really", "learning"]
+  array_1 << "arrays!"
 end
+p add_element_to_end_of_array(array_1, "arrays!")
 
 def add_element_to_start_of_array(array, element)
-  
+  array_2 = ["I", "am", "really", "learning"]
+  array_2.unshift("wow")
 end
+p add_element_to_start_of_array(array_2, "wow")
 
 def remove_element_from_end_of_array(array)
-  
+  create_an_array.pop
 end
+p remove_element_from_end_of_array(create_an_array)
 
 def remove_element_from_start_of_array(array)
-  
+  create_an_array.shift 
 end
+p remove_element_from_start_of_array(create_an_array)
 
 def retrieve_element_from_index(array, index_number)
   
