ad6ec764aeada786fc9af167779db3a8ee762a04
fortuitous-prototype-dfa5 <lindsay.chen@yale.edu>
2021-05-27 12:02:48 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,12 +7,11 @@ def create_an_array
 end
 p create_an_array
 
-array_1 = ["wow", "I", "am", "really", "learning"]
 def add_element_to_end_of_array(array, element)
   array_1 = ["wow", "I", "am", "really", "learning"]
   array_1 << "arrays!"
 end
-p add_element_to_end_of_array(array_1, "arrays!")
+p add_element_to_end_of_array "array_1", "arrays!"
 
 array_2 = ["I", "am", "really", "learning"]
 def add_element_to_start_of_array(array, element)
