ecaa983dbb0df2d9c6242ad00672bc0fe7817b14
fortuitous-prototype-dfa5 <lindsay.chen@yale.edu>
2021-05-27 11:31:39 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -9,13 +9,13 @@ p create_an_array
 
 array_1 = ["wow", "I", "am", "really", "learning"]
 def add_element_to_end_of_array(array, element)
-  array_1 << "arrays!"
+  #{array} << "arrays!"
 end
 p add_element_to_end_of_array(array_1, "arrays!")
 
 array_2 = ["I", "am", "really", "learning"]
 def add_element_to_start_of_array(array, element)
-  array_2.unshift("wow")
+  #{array}.unshift("wow")
 end
 p add_element_to_start_of_array(array_2, "wow")
 
